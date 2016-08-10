9891ea6ec0402db3acdf51001146471d5160fce8
Cheolho Jeon <adobecs5.kaist@gmail.com>
2016-08-10 18:19:14 +0900

    modifying ...
    -

--- backspace.js
+++ backspace.js
@@ -1,14 +1,32 @@
 document.onkeydown = checkKey;
+var val_of_this;
+var is_changing = false;
+
+var mutationObserver = new MutationObserver(
+  function () {console.log("changed!");}
+);
+
+mutationObserver.observe(document.activeElement, {
+    characterData: true,
+    attributeOldValue: true,
+    childList: true,
+    subtree: true
+  });
 
 function checkKey(e) {
     e = e || window.event;
-    // if backspace is pressed, go back.
+    console.log(e.keyCode);
+    // if backspace is pressed,
     if (e.keyCode == 8) {
       if (document.activeElement.tagName == 'INPUT'){
         // if on input, do nothing
-      } else{
-        //console.log(document.activeElement);
-        window.history.back();
+      } else {
+        // if not on input, go back.
+        //window.history.back();
       }
+    } else {
+      console.log(document.activeElement);
+      val_of_this = document.activeElement.innerHtml;
+      console.log(val_of_this);
     }
 }
